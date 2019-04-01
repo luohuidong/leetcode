dd86d2017c9e4101d99c08d7983a71b1b517b62c
luohuidong <luohuidong01@126.com>
2019-04-01 16:28:34 +0800

    557

---
+++ code/557/index.js
@@ -0,0 +1,8 @@
+export default (str) => {
+  const strArr = str.split(/\s/)
+  const result =  strArr.map(item => {
+    const charArr = [...item];
+    return charArr.reverse().join('')
+  }).join(' ')
+  return result
+}
