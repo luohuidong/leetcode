64d792bd5833595c97f06dc1754c5ef944545f73
luohuidong <luohuidong01@126.com>
2019-04-06 00:25:12 +0800

    新增 switchcase 缩进检查规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,38 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly',
-        'test': 'readonly',
-        'expect': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+  'env': {
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+    'test': 'readonly',
+    'expect': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'indent': ['error', 2, {
+      'SwitchCase': 1
+    }],
+  }
 }
