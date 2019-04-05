39cf9ffb54d115fd9c1dab15b134807e56a9d938
luohuidong <luohuidong01@126.com>
2019-04-05 22:37:11 +0800

    完善测试用例

--- code/914/test.js
+++ code/914/test.js
@@ -1,4 +1,17 @@
 import foo from '.'
 
-test('15, 10', () => {
+test('[1,2,3,4,4,3,2,1]', () => {
+  expect(foo([1,2,3,4,4,3,2,1])).toBe(true)
+})
+
+test('[1,1,1,2,2,2,3,3]', () => {
+  expect(foo([1,1,1,2,2,2,3,3])).toBe(false)
+})
+
+test('[1]', () => {
+  expect(foo([1])).toBe(false)
+})
+
+test('[1,1]', () => {
+  expect(foo([1,1])).toBe(true)
 })
