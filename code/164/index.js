a3dcacb20c29b6b9f5f7cc1b20e0b459674bc550
luohuidong <luohuidong01@126.com>
2019-04-09 00:30:44 +0800

    update

--- code/164/index.js
+++ code/164/index.js
@@ -1,15 +1,26 @@
-var maximumGap = function(nums) {
-  nums.sort((a, b) => a-b)
+var maximumGap = function (nums) {
+  if (nums.length < 2) {
+    return 0
+  }
+
   let maxDiff = 0
-  
-  for(let i = 0; i < nums.length - 1; i++) {
-    let diff = nums[i+1] - nums[i]
-    if (diff > maxDiff) {
-      maxDiff = diff
+
+  const len = nums.length
+
+  for (let i = len - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+      }
+    }
+    // 确保第一轮循环不计算差值
+    if (i < len - 1) {
+      maxDiff = nums[i + 1] - nums[i]
     }
   }
 
-  return maxDiff
+  // 由于在循环中，i 不会等于 0，所以在 return 中需补充这种情况。
+  return Math.max(maxDiff, nums[1] - nums[0])
 }
 
 export default maximumGap
